72eed6643f5b9e5e418e15e12fbf1beef5b6da71
TheMiddleGoesTeam <themiddlegoesteam@gmail.com>
2024-06-18 00:44:05 +0900

    유미씨 작업

---
+++ yumi/src/main/java/com/vo/MemberVO.java
@@ -0,0 +1,85 @@
+package com.vo;
+
+public class MemberVO {
+	private String mNum;
+	private String mID;
+	private String mNic;
+	private String mPass;
+	private String mName;
+	private String mtel1;
+	private String mtel2;
+	private String mtel3;
+	private String mEmail;
+	private int mMileage;
+	private String mCat;
+	public String getmNum() {
+		return mNum;
+	}
+	public void setmNum(String mNum) {
+		this.mNum = mNum;
+	}
+	public String getmID() {
+		return mID;
+	}
+	public void setmID(String mID) {
+		this.mID = mID;
+	}
+	public String getmNic() {
+		return mNic;
+	}
+	public void setmNic(String mNic) {
+		this.mNic = mNic;
+	}
+	public String getmPass() {
+		return mPass;
+	}
+	public void setmPass(String mPass) {
+		this.mPass = mPass;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	
+	public String getmEmail() {
+		return mEmail;
+	}
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+	public int getmMileage() {
+		return mMileage;
+	}
+	public void setmMileage(int mMileage) {
+		this.mMileage = mMileage;
+	}
+	public String getmCat() {
+		return mCat;
+	}
+	public void setmCat(String mCat) {
+		this.mCat = mCat;
+	}
+	public String getMtel1() {
+		return mtel1;
+	}
+	public void setMtel1(String mtel1) {
+		this.mtel1 = mtel1;
+	}
+	public String getMtel2() {
+		return mtel2;
+	}
+	public void setMtel2(String mtel2) {
+		this.mtel2 = mtel2;
+	}
+	public String getMtel3() {
+		return mtel3;
+	}
+	public void setMtel3(String mtel3) {
+		this.mtel3 = mtel3;
+	}
+
+	
+
+}
